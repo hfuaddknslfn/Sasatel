91138a4ffb74a322028b4d543c358e0923086250
hfuaddknslfn <anavnaana70@gmail.com>
2023-08-17 17:41:13 +0600

    v1

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'apple-iphone-3d-landing-page',
+  webDir: 'build',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
